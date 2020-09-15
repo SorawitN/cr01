5927787b3bb920683230981e30b8e6a7f1f63ea1
SorawitN <sorawit.nu@ku.th>
2020-09-16 03:21:49 +0700

    ep15: extract CourseReview from App add Login and About

--- cr-frontend/src/App.tsx
+++ cr-frontend/src/App.tsx
@@ -1,77 +1,19 @@
 import React from 'react';
-import './App.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
 
-import { Course } from './interface';
-import CourseItem from './component/CourseItem';
-import NewCourseForm from './component/NewCourseForm';
-import CourseService from './services/CourseService';
+import CourseReview from './component/CourseReview';
+import LoginForm from './component/LoginForm';
+import About from './component/About';
+
+
 
 const App = () => {
-  const [courses, setCourse] = useState<Course[]>([]);
-  const [formVisible, setFormVisible] = useState<boolean>(false);
-
-  const toggleFormVisible = () => {
-    setFormVisible(!formVisible);
-  }
-
-  const fetchCourse = () => {
-    CourseService.fetchCourse()
-    .then(courses => {
-      setCourse(courses);
-    });
-  };
-
-  const handleNewCourseCreated = (course: Course) => {
-    fetchCourse();
-    setFormVisible(false);
-  }
-
-  useEffect(() => {
-    fetchCourse();
-  },[]);
-
   return (
     <div className="App">
-      <ul>
-        {courses.map(item => (
-          <CourseItem key={item.id} course={item} />
-        ))}
-      </ul>
-      <button onClick={toggleFormVisible}>New Course</button>
-      {formVisible && 
-        <NewCourseForm onNewCourseCreate={handleNewCourseCreated}/>
-      }
+      <CourseReview />
+      <LoginForm />
+      <About />
     </div>
   );
 } 
-
-/* Class component method
-type AppState = {
-  message: string;
-};
-
-class App extends React.Component<{}, AppState> {
-  state: AppState = {
-    message: 'Default message',
-  };
-
-  componentDidMount(){
-    fetch('http://localhost:3000/frozen/')
-    .then(res => res.json())
-    .then(obj => {
-      this.setState({message: obj.message});
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.message}
-      </div>
-    );
-  }
-}  */
 
 export default App;
---
+++ cr-frontend/src/component/CourseReview.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+import { Course } from '../interface';
+import CourseItem from './CourseItem';
+import NewCourseForm from '../component/NewCourseForm';
+
+
+import CourseService from '../services/CourseService';
+
+const CourseReview = () => {
+  const [courses, setCourse] = useState<Course[]>([]);
+  const [formVisible, setFormVisible] = useState<boolean>(false);
+
+  const toggleFormVisible = () => {
+    setFormVisible(!formVisible);
+  }
+
+  const fetchCourse = () => {
+    CourseService.fetchCourse()
+    .then(courses => {
+      setCourse(courses);
+    });
+  };
+
+  const handleNewCourseCreated = (course: Course) => {
+    fetchCourse();
+    setFormVisible(false);
+  }
+
+  useEffect(() => {
+    fetchCourse();
+  },[]);
+
+  return (
+    <div className="CourseReview">
+      <ul>
+        {courses.map(item => (
+          <CourseItem key={item.id} course={item} />
+        ))}
+      </ul>
+      <button onClick={toggleFormVisible}>New Course</button>
+      {formVisible && 
+        <NewCourseForm onNewCourseCreate={handleNewCourseCreated}/>
+      }
+    </div>
+  );
+} 
+
+export default CourseReview;
